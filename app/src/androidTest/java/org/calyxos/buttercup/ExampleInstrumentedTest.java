CalyxOS/platform_packages_apps_Firewall

Create project in Android Studio
* Java, empty activity, minSdk 30 (since this is a platform app anyway)

Change-Id: I078b862fa98cfc820a38aaa3a12b9af1bb1a64d2

app/src/androidTest/java/org/calyxos/firewall/ExampleInstrumentedTest.java
@@ -1,0 +1,26 @@
+package org.calyxos.firewall;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("org.calyxos.firewall", appContext.getPackageName());
+    }
+}

app/src/main/java/org/calyxos/firewall/MainActivity.java
@@ -1,0 +1,14 @@
+package org.calyxos.firewall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

app/src/test/java/org/calyxos/firewall/ExampleUnitTest.java
@@ -1,0 +1,17 @@
+package org.calyxos.firewall;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}

CalyxOS/platform_packages_apps_Buttercup

app/src/androidTest/java/org/calyxos/buttercup/ExampleInstrumentedTest.java
@@ -1,0 +1,26 @@
+package org.calyxos.buttercup;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("org.calyxos.buttercup", appContext.getPackageName());
+    }
+}

app/src/main/java/org/calyxos/buttercup/MainActivity.java
@@ -1,0 +1,14 @@
+package org.calyxos.buttercup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

app/src/test/java/org/calyxos/buttercup/ExampleUnitTest.java
@@ -1,0 +1,17 @@
+package org.calyxos.buttercup;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}