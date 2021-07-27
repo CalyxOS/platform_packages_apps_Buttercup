c502c817d202471d2df737b27d8e02736c691483
Ese Udom <udomese@yahoo.com>
2021-07-27 14:39:50 +0100

    Implement Send Logcat button logic
    Change-Id: I84766f1b07be8487e8c037df7ee54b6a09009185

--- app/src/main/java/org/calyxos/buttercup/model/FeedbackViewModel.java
+++ app/src/main/java/org/calyxos/buttercup/model/FeedbackViewModel.java
@@ -1,6 +1,9 @@
 package org.calyxos.buttercup.model;
 
 import android.content.Context;
+import android.util.Base64;
+import android.util.Log;
+import android.util.Patterns;
 
 import androidx.lifecycle.ViewModel;
 
@@ -9,9 +12,15 @@ import org.calyxos.buttercup.network.Network;
 import org.calyxos.buttercup.network.RequestListener;
 import org.calyxos.buttercup.repo.Repository;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FeedbackViewModel extends ViewModel {
 
-    private Repository repo;
+    private static final String TAG = FeedbackViewModel.class.getSimpleName();
+    private final Repository repo;
 
     public FeedbackViewModel() {
         repo = new Repository();
@@ -23,6 +32,93 @@ public class FeedbackViewModel extends ViewModel {
                 repo.submitFeedback(subject, body, requestListener);
             } else requestListener.onValidationFailed(context.getString(R.string.subject_body_empty));
         } else requestListener.onInternetError();
+    }
+
+    public synchronized void submitLogcat(Context context, RequestListener requestListener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //TODO show progress in a Notification
+                //get logcat
+                String logcat = repo.getLogcat();
+                Log.d(TAG, "Logcat {\n" + logcat + "}");
+                if (logcat.isEmpty())
+                    requestListener.onValidationFailed(context.getString(R.string.logcat_not_retrieved));
+                else {
+                    //scrub logcat of personal information
+                    logcat = scrubLogcat(logcat);
+                    Log.d(TAG, "Scrubbed Logcat {" + logcat + "}");
+                    String fileName = writeLogcatToFile(context, logcat);
+                    String fileBase64 = getBase64(logcat);
+                    if (!logcat.isEmpty()) { //in case it returns empty for some reason
+                        repo.submitFeedbackWithAttachment("Logcat", "", fileName, fileBase64, requestListener);
+                    } else requestListener.onValidationFailed(context.getString(R.string.logcat_not_retrieved));
+                }
+            }
+        }).start();
+    }
+
+    private String scrubLogcat(String  logcat) {
+        //Note: Escape metacharacters of any new regex patterns with Pattern.quote() before adding it below
+
+        String gpsPattern = "[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?),\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)";
+        //gpsPattern = Pattern.quote(gpsPattern);
+
+        //Modified Patterns.PHONE.pattern() so that it matches only numbers with leading '+' as phone numbers.
+        // This exempts dates which we need
+        String phonePattern = "(\\+[0-9]+[\\- \\.]*)+" // +<digits><sdd>*
+                + "(\\([0-9]+\\)[\\- \\.]*)?"   // (<digits>)<sdd>*
+                + "([0-9][0-9\\- \\.]+[0-9])"; // <digit><digit|sdd>+<digit>
+
+        String wordPattern = "phone number|phonenumber|phone-number"; //TODO match this words to remove text that follows it just in case
+        //String emailPattern = "(" + Patterns.EMAIL_ADDRESS.pattern() + ")";
+        //phonePattern = "(" + phonePattern + ")";
+
+        //concatenate multiple patterns together to use at once on the input sequence //TODO improve this approach
+        //String regex = String.join("|", emailPattern, phonePattern/*, Patterns.WEB_URL.pattern(), gpsPattern*/);
+
+        Pattern pattern = Pattern.compile(Patterns.EMAIL_ADDRESS.pattern());
+        Matcher matcher = pattern.matcher(logcat);
+
+        String out = "";
+
+        while (matcher.find()) {
+            out = matcher.replaceAll(generateReplacement(matcher.group().length()));
+        }
+
+        //matcher.usePattern(Pattern.compile("\\b(" + phonePattern + ")\\b"));
+        pattern = Pattern.compile(phonePattern);
+        matcher = pattern.matcher(out);
+
+        while (matcher.find()) {
+            out = matcher.replaceAll(generateReplacement(matcher.group().length()));
+        }
+
+        return out.isEmpty() ? logcat : out;
+    }
+
+    private String generateReplacement(int length) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++)
+            builder.append("*");
+
+        return builder.toString();
+    }
+
+    private String writeLogcatToFile(Context context, String logcat) {
+        String filename = "logcat.txt";
+        try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+            fos.write(logcat.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.e(TAG, "Logcat File: " + context.getFilesDir() + "/" + filename);
+        return filename;
+    }
+
+    private String getBase64(String logcat) {
+        return Base64.encodeToString(logcat.getBytes(), Base64.DEFAULT);
     }
 
 }
--- app/src/main/java/org/calyxos/buttercup/repo/Repository.java
+++ app/src/main/java/org/calyxos/buttercup/repo/Repository.java
@@ -4,15 +4,25 @@ import android.util.Log;
 
 import org.calyxos.buttercup.Constants;
 import org.calyxos.buttercup.model.Ticket;
+import org.calyxos.buttercup.model.compat.ArticleAttachmentCompat;
 import org.calyxos.buttercup.model.compat.TicketArticleCompat;
 import org.calyxos.buttercup.model.compat.TicketCompat;
 import org.calyxos.buttercup.network.RequestListener;
 import org.calyxos.buttercup.network.RetrofitFactory;
 import org.calyxos.buttercup.network.WebServices;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -67,6 +77,46 @@ public class Repository {
         });
     }
 
+    public void submitFeedbackWithAttachment(String subject, String body, String fileName, String fileBase64, RequestListener listener) {
+        TicketCompat ticketCompat = new TicketCompat();
+        ticketCompat.setCustomer(Constants.ZAMMAD_CUSTOMER);
+        ticketCompat.setGroup("Users");
+        ticketCompat.setTitle(subject);
+        List<ArticleAttachmentCompat> attachs = new ArrayList<>();
+        attachs.add(new ArticleAttachmentCompat(fileName, fileBase64, Constants.MIME_TYPE_TEXT));
+        ticketCompat.setArticle(new TicketArticleCompat(subject, body, Constants.ARTICLE_TYPE, false, attachs));
+        webServices.createTicket(getHeaderMap(), ticketCompat).enqueue(new Callback<Ticket>() {
+            @Override
+            public void onResponse(Call<Ticket> call, Response<Ticket> response) {
+                if (listener != null) {
+                    if (response.isSuccessful()) {
+                        listener.onSuccess();
+                        assert response.body() != null;
+                        Log.d(TAG, response.body().toString());
+                    } else {
+                        listener.onFail(response.message());
+                        assert response.errorBody() != null;
+                        try {
+                            Log.e(TAG, "Error Body: " + response.errorBody().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Log.e(TAG, "Message: " + response.message());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Ticket> call, Throwable t) {
+                if(listener != null)
+                    listener.onConnectionError(t.getMessage());
+
+                Log.e(TAG, "Connection Error: " + t.getMessage());
+                t.printStackTrace();
+            }
+        });
+    }
+
     private Map<String, String> getHeaderMap() {
         Map<String, String> map = new HashMap<>();
         map.put(Constants.AUTHORIZATION, Constants.BEARER + Constants.TOKEN);
@@ -74,4 +124,45 @@ public class Repository {
         return map;
     }
 
+    public String getLogcat() {
+        StringBuilder log = new StringBuilder();
+        try {
+            Process process = Runtime.getRuntime().exec("logcat -b all -d");
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return insertCRs(log.toString());
+    }
+
+    /**
+     * Breaks up the logcat generated into single lines by inserting carriage return at the proper places which is before
+     * the dates.
+     * @param input string input
+     * @return string output
+     */
+    private String insertCRs(String input) {
+        String  datePattern = "(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) ";
+        Pattern pattern = Pattern.compile(datePattern);
+
+        Matcher matcher = pattern.matcher(input);
+
+        String out = input;
+        Set<String> matchedWords = new HashSet<>();
+        while (matcher.find()) {
+            matchedWords.add(matcher.group());
+        }
+
+        for (String word: matchedWords) {
+            out = out.replace(word, "\n" + word);
+        }
+
+        return out;
+    }
 }
