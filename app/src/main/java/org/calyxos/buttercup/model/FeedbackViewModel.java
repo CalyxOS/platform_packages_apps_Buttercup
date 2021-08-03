a56a1ca852c3c13240b60e26cdc05500f6f61793
Ese Udom <udomese@yahoo.com>
2021-08-03 15:09:09 +0100

    Add and improve new scrubbing regex patterns
    Change-Id: I14fa31714aacefe8e8034640c5325d1252f4d969

--- app/src/main/java/org/calyxos/buttercup/model/FeedbackViewModel.java
+++ app/src/main/java/org/calyxos/buttercup/model/FeedbackViewModel.java
@@ -12,8 +12,10 @@ import org.calyxos.buttercup.network.Network;
 import org.calyxos.buttercup.network.RequestListener;
 import org.calyxos.buttercup.repo.Repository;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -59,10 +61,9 @@ public class FeedbackViewModel extends ViewModel {
     }
 
     private String scrubLogcat(String  logcat) {
-        //Note: Escape metacharacters of any new regex patterns with Pattern.quote() before adding it below
+        //Note: Escape metacharacters of any new regex patterns manually or with Pattern.quote() before adding it below
 
         String gpsPattern = "[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?),\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)";
-        //gpsPattern = Pattern.quote(gpsPattern);
 
         //Modified Patterns.PHONE.pattern() so that it matches only numbers with leading '+' as phone numbers.
         // This exempts dates which we need
@@ -70,31 +71,67 @@ public class FeedbackViewModel extends ViewModel {
                 + "(\\([0-9]+\\)[\\- \\.]*)?"   // (<digits>)<sdd>*
                 + "([0-9][0-9\\- \\.]+[0-9])"; // <digit><digit|sdd>+<digit>
 
-        String wordPattern = "phone number|phonenumber|phone-number"; //TODO match this words to remove text that follows it just in case
+        String phonePattern1 = "(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?" +
+                "(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)" +
+                "|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*" +
+                "(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?"; //not a suitable pattern for this. Needs improvement perhaps
+
+        String emailPattern1 = "[a-zA-Z0-9_]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*" +
+                "(@|%40)(?!([a-zA-Z0-9]*\\.[a-zA-Z0-9]*\\.[a-zA-Z0-9]*\\.))(?:[A-Za-z0-9](?:[a-zA-Z0-9-]*[A-Za-z0-9])?\\.)+" +
+                "[a-zA-Z](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
+
+        String webURLPattern = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+        String webURLPattern1 = "\\b(www)\\.[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"; //matches domains like www.twitter.com
+
+        String ipAddressPattern = "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                "([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
+
+        String phoneInfoPattern = "(msisdn=|mMsisdn=|iccid=|iccid: |mImsi=)[a-zA-Z0-9]*";
+
+        String userInfoPattern = "(UserInfo\\{\\d:)[a-zA-Z0-9\\s]*";
+
+        String acctInfoPattern = "(Account \\{name=)[a-zA-Z0-9]*";
+
+        //concatenate multiple patterns together to use at once on the input sequence //TODO improve this approach. Test.
+
         //String emailPattern = "(" + Patterns.EMAIL_ADDRESS.pattern() + ")";
         //phonePattern = "(" + phonePattern + ")";
+        //phonePattern1 = "(" + phonePattern1 + ")"; //Note: before using this pattern in this way, remove start and end symbols
+        //emailPattern1 = "(" + emailPattern1 + ")";
+        //webURLPattern = "(" + webURLPattern + ")";
+        //ipAddressPattern = "(" + ipAddressPattern + ")"; //Note: before using this pattern in this way, remove start and end symbols
+        //phoneInfoPattern = "(" + phoneInfoPattern + ")";
+        //userInfoPattern = "(" + userInfoPattern + ")";
+        //acctInfoPattern = "(" + acctInfoPattern + ")";
 
-        //concatenate multiple patterns together to use at once on the input sequence //TODO improve this approach
-        //String regex = String.join("|", emailPattern, phonePattern/*, Patterns.WEB_URL.pattern(), gpsPattern*/);
+        //String regex = String.join("|", emailPattern, phonePattern, webURLPattern, ipAddressPattern, phoneInfoPattern, acctInfoPattern,
+        // gpsPattern);
 
-        Pattern pattern = Pattern.compile(Patterns.EMAIL_ADDRESS.pattern());
-        Matcher matcher = pattern.matcher(logcat);
+        Pattern emPattern = Pattern.compile(Patterns.EMAIL_ADDRESS.pattern());
+        Pattern emPattern1 = Pattern.compile(emailPattern1);
+        Pattern phPattern = Pattern.compile(phonePattern);
+        Pattern phPattern1 = Pattern.compile(phonePattern1);
+        Pattern webPattern = Pattern.compile(webURLPattern);
+        Pattern webPattern1 = Pattern.compile(webURLPattern1);
+        Pattern ipPattern = Pattern.compile(ipAddressPattern);
+        Pattern piPattern = Pattern.compile(phoneInfoPattern, Pattern.CASE_INSENSITIVE);
+        Pattern uiPattern = Pattern.compile(userInfoPattern, Pattern.CASE_INSENSITIVE);
+        Pattern aiPattern = Pattern.compile(acctInfoPattern, Pattern.CASE_INSENSITIVE);
 
-        String out = "";
 
-        while (matcher.find()) {
-            out = matcher.replaceAll(generateReplacement(matcher.group().length()));
-        }
+        logcat = emPattern1.matcher(logcat).replaceAll("***EMAIL***");
+        logcat = phPattern.matcher(logcat).replaceAll("***PHONE***");
+        logcat = webPattern.matcher(logcat).replaceAll("***WEB-URL***");
+        logcat = webPattern1.matcher(logcat).replaceAll("***WEB-URL***");
+        logcat = ipPattern.matcher(logcat).replaceAll("***IP***");
+        logcat = piPattern.matcher(logcat).replaceAll("***PHONE-INFO***");
+        logcat = uiPattern.matcher(logcat).replaceAll("***USER-INFO***");
+        logcat = aiPattern.matcher(logcat).replaceAll("***ACCT-INFO***");
 
         //matcher.usePattern(Pattern.compile("\\b(" + phonePattern + ")\\b"));
-        pattern = Pattern.compile(phonePattern);
-        matcher = pattern.matcher(out);
 
-        while (matcher.find()) {
-            out = matcher.replaceAll(generateReplacement(matcher.group().length()));
-        }
-
-        return out.isEmpty() ? logcat : out;
+        return logcat;
     }
 
     private String generateReplacement(int length) {
